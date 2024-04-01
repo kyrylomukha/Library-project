63874c11d578862131631f59e03ee819dd83520d
kyrylomukha <111917832+kyrylomukha@users.noreply.github.com>
2024-04-01 21:05:07 +0300

    Add full functonality to HTML dialog window

--- main.js
+++ main.js
@@ -1,20 +1,56 @@
-const myLibrary = [];
-
 const newBook = document.querySelector("#newBook");
 const dialog = document.querySelector(".dialog");
+const closeDialog = document.querySelector("#close-button");
+const confirmBtn = document.querySelector("#confirm-button");
+const libraryWindow = document.querySelector(".library");
+
+const myLibrary = [];
 
 newBook.addEventListener("click", () => {
   dialog.showModal();
 });
 
+closeDialog.addEventListener("click", () => {
+  dialog.close();
+})
+
+confirmBtn.addEventListener("click",(event) => {
+  event.preventDefault();
+  handleConfirm();
+  dialog.close();
+})
+
+function handleConfirm() {
+  const titleValue = document.querySelector("#bookTitle").value;
+  const authorValue = document.querySelector("#bookAuthor").value;
+  const pagesValue = document.querySelector("#bookPages").value;
+  const readValue = document.querySelector("#isRead").checked;
+
+  let isRead;
+  if(readValue) {
+    isRead = true;
+  } else {
+    isRead = false;
+  }
+  
+  const book = new Book(titleValue,authorValue,pagesValue,isRead);
+  myLibrary.push(book);
+  createBookCard();
+}
+
 function Book(title, author, pages, read) {
   this.title = title,
   this.author = author,
   this.pages = pages,
-  this.read = read,
-  this.isRead = this.read ? "read" : "not read yet"
+  this.read = read
 };
 
-function addBookToLibrary() {
+function createBookCard() {
+  const card = document.createElement("div");
+  card.classList.add(".card");
+  libraryWindow.appendChild(card);
+}
+
+function addBookToLibrary(book) {
 
 }
