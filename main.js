9e6a99a93464061b1b98481c45dfd36c5f2aa5b5
kyrylomukha <111917832+kyrylomukha@users.noreply.github.com>
2024-04-01 19:07:45 +0300

    Add dialog window with basic functionality

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
 const myLibrary = [];
 
+const newBook = document.querySelector("#newBook");
+const dialog = document.querySelector(".dialog");
+
+newBook.addEventListener("click", () => {
+  dialog.showModal();
+});
+
 function Book(title, author, pages, read) {
   this.title = title,
   this.author = author,
   this.pages = pages,
   this.read = read,
-  this.readInfo = this.read ? "read" : "not read yet"
+  this.isRead = this.read ? "read" : "not read yet"
 };
 
 function addBookToLibrary() {
-  
+
 }
